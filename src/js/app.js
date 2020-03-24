ab8fa44801369892e2cd0343e231b36e996debf8
rafalstrozyk <rafal.strozyk.biz@gmail.com>
2020-03-24 23:18:58 +0100

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,3 @@
+console.log("Hello");
+
+const d = 5;
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: ['./src/js/app.js'],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/app.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+            }
+        ]
+    }
+}
