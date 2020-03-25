0a393cd3ff450fb1b7605f1d55431d2560574aae
rafalstrozyk <rafal.strozyk.biz@gmail.com>
2020-03-25 16:32:46 +0100

    Add new-list-box

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,7 @@
-console.log("Hello");
+document.querySelector(".new-list-box__primary--text").addEventListener('focus',() => {
+    document.querySelector(".new-list-box__options").style.opacity = "1";
+});
+document.getElementById("btn-add").addEventListener('click',() => {
+    document.querySelector(".new-list-box__options").style.opacity = "0";
+});
 
-const d = 5;
